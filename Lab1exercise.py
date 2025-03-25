2cc9e1c3d2900d7c3e7bf74e1118d28cde5d5247
George K Kimari <gkkimari@usiu.ac.ke>
2025-03-25 15:25:54 +0300

    Added assignment files

---
+++ Lab1exercise.py
@@ -0,0 +1,11 @@
+#This is a personalazised greeting program  
+#Prompt to ask for the users name
+username= input("Enter your name")
+#This is to ask for the users age
+userage= input("Enter your age")
+#This is to display a personalized greeting to the user
+print(f"Hello, {username}! Nice to meet you")
+#This is is to display a message about their age
+print(f"You age {userage} years old. That is awesome.")
+print("WELCOME TO YOUR PYTHON PROGRAMMING JOURNEY")
+
